65cea7734d508013ba2e4a64d7dd2e4332635640
marcoDevUni <marco.bellotti4@edu.unito.it>
2022-08-20 17:39:39 +0200

    avanzamento porting

--- src/prova.py
+++ src/prova.py
@@ -1,3 +1,4 @@
+from tabnanny import verbose
 from scapy.all import *
 import sys
 sys.path.append('./scapy/layers/')
@@ -6,17 +7,16 @@ import ssh
 
 
 def a(myIP):
-    p=IP(dst=myIP)/TCP(dport=22)/ssh.SSH()/ssh.SSHIdent(ident="SSH-2.0-x\r\n")
-
-    p.show()
+    p=IP(dst=myIP)/TCP(dport=22)/ssh.SSH()/ssh.SSHIdent(ident="SSH-2.0-\r\n")
     return sr(p)   
 
-def b(myIP):
-    p=IP(dst=myIP)/TCP(dport=22)/ssh.SSHMessage()/ssh.SSHKexInit(languages_client_to_server="uk",reserved=0)
+# def b(myIP):
+#     p=IP(dst=myIP)/TCP(dport=22)/ssh.SSHMessage()/ssh.SSHKexInit(languages_client_to_server="uk",reserved=0)
+#     p.show()
+#     return sr(p)
 
-    p.show()
-    return sr(p)    
 myIP="127.0.0.1"
 x,y=a(myIP)
-ssh.SSH(x).show()
-ssh.SSH(y).show()
+
+print(x)
+print(y)
